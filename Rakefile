3af5d99b80ac141e2995b4d99188c521880a614e
Tim Schmelmer <tim.schmelmer@gmail.com>
2012-05-13 09:44:36 -0600

    first playable version with sound fx; TODO next: better/correct random button algorithm

--- Rakefile
+++ Rakefile
@@ -4,5 +4,6 @@ require 'motion/project'
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'CalcGame'
+  app.frameworks << 'AVFoundation'
   app.icons << 'icon.png'
 end
--- app/controllers/calc_game_view_controller.rb
+++ app/controllers/calc_game_view_controller.rb
@@ -11,6 +11,7 @@ class CalcGameViewController < UIViewController
   def viewDidLoad
     view.image = UIImage.imageNamed('background.jpg')
     view.userInteractionEnabled = true
+    load_sound_effects
 
     add_equation_and_number_buttons
 
@@ -33,8 +34,11 @@ class CalcGameViewController < UIViewController
   end
 
   def make_random_buttons
+    winner_pos = rand(BUTTONS)
     (0...BUTTONS).each do |pos|
-      number_buttons <<  make_button(pos, withValue: (rand(BUTTONS) + 1))
+      button =  (pos == winner_pos) ? make_button(pos, withValue: equation.result, forSuccess: true) : make_button(pos, withValue: (rand(BUTTONS) + 1), forSuccess: false)
+      puts "YAY: #{button.currentTitle}"
+      number_buttons << button
     end
     number_buttons
   end
@@ -57,26 +61,32 @@ class CalcGameViewController < UIViewController
   end
 
 
-  def make_button(pos, withValue:val)
+  def make_button(pos, withValue:val, forSuccess:success)
+    action_for_button = success ? "action_tapped_happy:" : "action_tapped_sad:"
     margin = 10
     width = height = (view.frame.size.width - margin * 4) / 3
     top_y = 150
     button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
     button.setTitle(val.to_s, forState:UIControlStateNormal)
-    button.addTarget(self, action:"action_tapped:", forControlEvents:UIControlEventTouchUpInside)
+    button.addTarget(self, action: action_for_button , forControlEvents:UIControlEventTouchUpInside)
     button.frame = [[(pos % 3) * (margin + width) + margin, top_y + ((pos / 3) * (margin + height)) ], [width, height]]
     button
   end
 
-  def action_tapped(sender)
-    UIView.animateWithDuration(1.0,
+  def action_tapped_sad(sender)
+    @fail_sound.play
+  end
+
+  def action_tapped_happy(sender)
+    UIView.animateWithDuration(1.25,
                                animations:lambda {
                                    @equation.label.alpha = 0
                                    @equation.label.transform = CGAffineTransformMakeScale(0.1, 0.1)
+                                   @applause_sound.play
                                },
                                completion:lambda { |finished|
                                    @equation.label.text = @equation.label.text.gsub(/\?/, sender.currentTitle)
-                                   UIView.animateWithDuration(1.0,
+                                   UIView.animateWithDuration(6,
                                                     animations:lambda {
                                                         @equation.label.alpha = 1
                                                         @equation.label.transform = CGAffineTransformIdentity
@@ -88,4 +98,19 @@ class CalcGameViewController < UIViewController
                                })
   end
 
+  def load_sound_effects
+    @fail_sound =     load_caf_sound 'fail-buzzer'
+    @applause_sound = load_caf_sound 'applause'
+  end
+  
+  def load_caf_sound(name)
+    path = NSBundle.mainBundle.pathForResource(name, ofType:'caf')
+    url = NSURL.fileURLWithPath(path)
+    error_ptr = Pointer.new(:id)
+    sound = AVAudioPlayer.alloc.initWithContentsOfURL(url,error:error_ptr)
+    unless sound
+      raise "Can't open sound file: #{error_ptr[0].description}"
+    end
+    sound
+  end
 end
